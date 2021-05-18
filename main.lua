1ebca686d820cf4f106daa9309349edd8e1369e9
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2021-05-18 11:47:12 -0300

    update-final

--- main.lua
+++ main.lua
@@ -75,6 +75,8 @@ function love.load()
     gStateMachine:change('title')
 
     love.keyboard.keysPressed = {}
+
+    love.mouse.buttonsPressed = {}
 end
 
 function love.resize(w, h)
@@ -89,12 +91,16 @@ function love.keypressed(key)
     end
 end
 
+function love.mousepressed(x, y, button)
+    love.mouse.buttonsPressed[button] = true
+end
+
 function love.keyboard.wasPressed(key)
-    if love.keyboard.keysPressed[key] then
-        return true
-    else
-        return false
-    end
+    return love.keyboard.keysPressed[key]
+end
+
+function love.mouse.wasPressed(button)
+    return love.mouse.buttonsPressed[button]
 end
 
 function love.update(dt)
@@ -107,6 +113,7 @@ function love.update(dt)
     gStateMachine:update(dt)
     
     love.keyboard.keysPressed = {}
+    love.mouse.buttonsPressed = {}
 end
 
 function love.draw()
